294fde5d77e58eb360cf606987e26bb646007240
noobtree <tree4961@daum.net>
2026-06-30 22:26:25 +0900

    [차지훈] Crosshair를 담당하는 UI 클래스 추가

---
+++ RoomEscape/Source/RoomEscape/Private/UI/CrosshairWidget.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/CrosshairWidget.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Components/HorizontalBox.h"
+#include "UI/InputActionPromptWidget.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+
+void UCrosshairWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	//Slot->
+}
+
+void UCrosshairWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+}
+
+void UCrosshairWidget::SetObjectInfo(UTexture2D* Texture_ObjectIcon, const FText& ObjectName)
+{
+	// 바라보고 있는 객체를 나타내는 UI의 아이콘 및 크기 설정
+	if (IsValid(Image_ObjectIcon) == true && IsValid(Texture_ObjectIcon) == true)
+	{
+		Image_ObjectIcon->SetBrushFromTexture(Texture_ObjectIcon);
+		Image_ObjectIcon->SetBrushSize({ 32, 32 });
+	}
+
+	// 바라보고 있는 객체 이름을 나타내는 TextBlock 내용 설정
+	if (IsValid(TextBlock_ObjectName) == true && ObjectName.IsEmpty() == false)
+	{
+		TextBlock_ObjectName->SetText(ObjectName);
+	}
+}
+
+void UCrosshairWidget::SetInputActionNavigationBar(const TArray<FInputActionPromptData>& InputActionPrompts)
+{
+	// 기존의 InputAction NavigationBar 초기화
+	HorizontalBox_InputActionNavigation->ClearChildren();
+
+	// 순서대로 사용자 입력에 대한 Widget 생성
+	for (const FInputActionPromptData& InputActionPrompt : InputActionPrompts)
+	{
+		// 사용자 입력에 대한 Widget 생성
+		UInputActionPromptWidget* IndicatorWidget = CreateWidget<UInputActionPromptWidget>(GetOwningPlayer(), InputActionPromptWidgetClass);
+		if (IsValid(IndicatorWidget) == false)
+		{
+			continue;
+		}
+
+		// Widget 초기 설정 진행
+		IndicatorWidget->InitializeIndicatorPrompt(InputActionPrompt);
+
+		// 내비게이션 영역에 Widget 추가
+		HorizontalBox_InputActionNavigation->AddChildToHorizontalBox(IndicatorWidget);
+	}
+}
+
+void UCrosshairWidget::SetCrosshairToCenter(const FVector2D& CurrentSize)
+{
+	if (IsValid(Image_Crosshair) == false)
+	{
+		PreviousSize = CurrentSize;
+		return;
+	}
+
+	PreviousSize = CurrentSize;
+	return;
+}
---
+++ RoomEscape/Source/RoomEscape/Public/UI/CrosshairWidget.h
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CrosshairWidget.generated.h"
+
+USTRUCT(BlueprintType)
+struct ROOMESCAPE_API FInputActionPromptData
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UTexture2D> Texture_InputAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText DisplayActionName;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMESCAPE_API UCrosshairWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+protected:
+#pragma region Widget Components
+
+	// 포커싱 된 오브젝트의 아이콘을 출력하는 Image
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<class UImage> Image_ObjectIcon;
+
+	// 포커싱 된 오브젝트의 이름을 출력하는 TextBlock
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<class UTextBlock> TextBlock_ObjectName;
+
+	// 포커싱 상태에 맞추어 Crosshair를 출력하는 Image
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<class UImage> Image_Crosshair;
+
+	// InputAction Prompt를 나타내는 영역
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<class UHorizontalBox> HorizontalBox_InputActionNavigation;
+
+#pragma endregion
+
+	// Crosshair 이미지 하단에 노출되는 InputAction Prompt의 단위별 Widget 클래스
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUserWidget> InputActionPromptWidgetClass;
+
+private:
+	UPROPERTY()
+	FVector2D PreviousSize = FVector2D::ZeroVector;
+
+public:
+	// Crosshair 상단에 노출되는 객체의 아이콘, 이름 설정하는 함수
+	UFUNCTION(BlueprintCallable)
+	void SetObjectInfo(UTexture2D* Texture_ObjectIcon, const FText& ObjectName);
+
+	// Crosshair 하단에 노출되는 InputAction Navigation 내용을 설정하는 함수
+	UFUNCTION(BlueprintCallable)
+	void SetInputActionNavigationBar(const TArray<FInputActionPromptData>& InputActionPrompts);
+
+protected:
+	// !!! Not Completed !!! Don't Use
+	// Crosshair 이미지가 화면 정중앙에 위치할 수 있도록 Widget의 위치를 조정하는 함수
+	void SetCrosshairToCenter(const FVector2D& CurrentSize);
+};
