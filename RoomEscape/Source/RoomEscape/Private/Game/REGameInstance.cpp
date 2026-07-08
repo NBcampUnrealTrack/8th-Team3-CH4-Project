9d4efda873b73e61f1f7adcec2429b2d80ec0167
DevHN <devhn0@gmail.com>
2026-07-08 21:02:10 +0900

    [승재] 서버 진입 기능 수정

--- RoomEscape/Source/RoomEscape/Private/Game/REGameInstance.cpp
+++ RoomEscape/Source/RoomEscape/Private/Game/REGameInstance.cpp
@@ -42,7 +42,7 @@ void UREGameInstance::SetItemDataTable(UDataTable* NewItemDataTable)
 
 // -- 방 만들기 --
 
-void UREGameInstance::HostGame()
+void UREGameInstance::HostGame(FString RoomName, FString Password)
 {
 	if (TSharedPtr<IOnlineSession> SharedSession = SessionInterface.Pin())
 	{
@@ -51,6 +51,9 @@ void UREGameInstance::HostGame()
 		SessionSettings.NumPublicConnections = 2;	// 2인 제한
 		SessionSettings.bShouldAdvertise = true;
 		SessionSettings.bUsesPresence = true;
+		
+		SessionSettings.Set(FName("SERVER_NAME"), RoomName, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
+		SessionSettings.Set(FName("ROOM_PASSWORD"), Password, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
 		
 		SharedSession->CreateSession(0, FName("RoomEscapeSession"), SessionSettings);
 	}
@@ -66,28 +69,45 @@ void UREGameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSucces
 
 // -- 방 찾기 --
 
-void UREGameInstance::JoinGame()
+void UREGameInstance::JoinGame(FString InputRoomName, FString InputPassword)
 {
+	TargetRoomName = InputRoomName;
+	TargetPassword = InputPassword;
+
 	if (TSharedPtr<IOnlineSession> SharedSession = SessionInterface.Pin())
 	{
 		SessionSearch = MakeShareable(new FOnlineSessionSearch());
-		SessionSearch->bIsLanQuery = true;			// LAN 환경에서 검색
+		SessionSearch->bIsLanQuery = true;       
 		SessionSearch->MaxSearchResults = 100;
-		
+       
 		SharedSession->FindSessions(0, SessionSearch.ToSharedRef());
 	}
 }
 
 void UREGameInstance::OnFindSessionsComplete(bool bWasSuccessful)
 {
-	if (bWasSuccessful && SessionSearch->SearchResults.Num() > 0)
+	if (bWasSuccessful && SessionSearch.IsValid())
 	{
 		if (TSharedPtr<IOnlineSession> SharedSession = SessionInterface.Pin())
 		{
-			// 가장 먼저 찾은 방에 접속 시도
-			SharedSession->JoinSession(0, FName("EscapeSession"), SessionSearch->SearchResults[0]);
+			for (const FOnlineSessionSearchResult& Result : SessionSearch->SearchResults)
+			{
+				FString FoundName;
+				FString FoundPassword;
+
+				Result.Session.SessionSettings.Get(FName("SERVER_NAME"), FoundName);
+				Result.Session.SessionSettings.Get(FName("ROOM_PASSWORD"), FoundPassword);
+
+				if (FoundName == TargetRoomName && FoundPassword == TargetPassword)
+				{
+					SharedSession->JoinSession(0, FName("RoomEscapeSession"), Result);
+					return; 
+				}
+			}
 		}
 	}
+
+	OnJoinProcessResult.Broadcast(false, TEXT("방 이름이나 비밀번호가 일치하지 않습니다."));
 }
 
 void UREGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
@@ -97,15 +117,20 @@ void UREGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCom
 		if (TSharedPtr<IOnlineSession> SharedSession = SessionInterface.Pin())
 		{
 			FString JoinAddress;
-			
+          
 			if (SharedSession->GetResolvedConnectString(SessionName, JoinAddress))
 			{
-				// 주소를 성공적으로 획득했다면 해당 주소의 서버로 클라이언트 Travel
+				OnJoinProcessResult.Broadcast(true, TEXT("접속 성공! 게임으로 이동합니다."));
+
 				if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
 				{
 					PlayerController->ClientTravel(JoinAddress, ETravelType::TRAVEL_Absolute);
 				}
 			}
 		}
+	}
+	else
+	{
+		OnJoinProcessResult.Broadcast(false, TEXT("접속에 실패했습니다."));
 	}
 }
