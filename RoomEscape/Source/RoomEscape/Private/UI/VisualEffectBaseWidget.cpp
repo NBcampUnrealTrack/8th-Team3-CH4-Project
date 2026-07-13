8207e3d627f1dd20de54f99bb1f66c190d5c291c
noobtree <tree4961@daum.net>
2026-07-13 10:49:36 +0900

    [차지훈] FadeIn, FadeOut 담당 UI 클래스 추가

---
+++ RoomEscape/Source/RoomEscape/Private/UI/VisualEffectBaseWidget.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/VisualEffectBaseWidget.h"
+#include "Animation/WidgetAnimation.h"
+
+void UVisualEffectBaseWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (IsValid(FadeInAnimation) == true)
+	{
+		FadeInAnimationFinishedDelegate.BindDynamic(this, &ThisClass::HandleFadeInAnimationFinished);
+		BindToAnimationFinished(FadeInAnimation, FadeInAnimationFinishedDelegate);
+	}
+
+	if (IsValid(FadeOutAnimation) == true)
+	{
+		FadeOutAnimationFinishedDelegate.BindDynamic(this, &ThisClass::HandleFadeOutAnimationFinished);
+		BindToAnimationFinished(FadeOutAnimation, FadeOutAnimationFinishedDelegate);
+	}
+}
+
+void UVisualEffectBaseWidget::NativeOnActivated()
+{
+	Super::NativeOnActivated();
+
+	PlayAnimation(FadeInAnimation);
+}
+
+void UVisualEffectBaseWidget::HandleFadeInAnimationFinished()
+{
+	OnWidgetFadeIn.Broadcast();
+}
+
+void UVisualEffectBaseWidget::HandleFadeOutAnimationFinished()
+{
+	OnWidgetFadeOut.Broadcast();
+
+	// Widget 비활성화
+	DeactivateWidget();
+}
---
+++ RoomEscape/Source/RoomEscape/Public/UI/VisualEffectBaseWidget.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "VisualEffectBaseWidget.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWidgetFadeInOutSignature);
+
+/**
+ * 
+ */
+UCLASS()
+class ROOMESCAPE_API UVisualEffectBaseWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+
+protected:
+	virtual void NativeOnActivated() override;
+
+public:
+	// Widget이 FadeIn 되었을 경우 실행되는 이벤트
+	UPROPERTY(BlueprintAssignable)
+	FOnWidgetFadeInOutSignature OnWidgetFadeIn;
+
+	// Widget이 FadeOut 되었을 경우 실행되는 이벤트
+	UPROPERTY(BlueprintAssignable)
+	FOnWidgetFadeInOutSignature OnWidgetFadeOut;
+	
+protected:
+	// FadeIn 애니메이션
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BindWidgetAnim, AllowPrivateAccess = "true"), Transient)
+	TObjectPtr<UWidgetAnimation> FadeInAnimation;
+
+	// FadeOut 애니메이션
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (BindWidgetAnim, AllowPrivateAccess = "true"), Transient)
+	TObjectPtr<UWidgetAnimation> FadeOutAnimation;
+
+	FWidgetAnimationDynamicEvent FadeInAnimationFinishedDelegate;
+
+	FWidgetAnimationDynamicEvent FadeOutAnimationFinishedDelegate;
+
+public:
+	UFUNCTION()
+	void HandleFadeInAnimationFinished();
+
+	UFUNCTION()
+	void HandleFadeOutAnimationFinished();
+};
